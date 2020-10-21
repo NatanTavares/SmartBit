994ce4421aec941af4a2610b534ca4f72b029abc
NatanTavares <nattantavares.s15@gmail.com>
2020-10-21 12:31:57 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import SectionHeader from './components/SectionHeader/SectionHeader';
+
+function App() {
+  return (
+    <main className="App">
+      <SectionHeader /> 
+    </main>
+  );
+}
+
+export default App;
---
+++ web/src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button () {
+  return (
+    <button>
+      I'm a button!
+    </button>
+  );
+}
+
+export default Button;
